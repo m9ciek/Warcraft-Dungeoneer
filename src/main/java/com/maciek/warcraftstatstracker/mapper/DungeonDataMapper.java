8f54454ca514aeede7eaa6f2e2aee07182eeb818
m9ciek <ahubu123@gmail.com>
2020-09-07 14:04:43 +0200

    Object mapping moved from DungeoneerService to DungonDataMapper

---
+++ src/main/java/com/maciek/warcraftstatstracker/mapper/DungeonDataMapper.java
@@ -0,0 +1,79 @@
+package com.maciek.warcraftstatstracker.mapper;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.maciek.warcraftstatstracker.model.dungeoneer.DungeonData;
+import com.maciek.warcraftstatstracker.model.dungeoneer.KeystoneAffix;
+import com.maciek.warcraftstatstracker.model.dungeoneer.MythicPlusDungeon;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.ZoneId;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class DungeonDataMapper {
+
+    public static DungeonData mapJSONToDungeonData(String jsonData) throws JsonProcessingException {
+        DungeonData dungeonData = new DungeonData();
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonNode node = objectMapper.readValue(jsonData, JsonNode.class);
+
+        dungeonData.setSeason(node.get("season").get("id").asInt());
+        List<MythicPlusDungeon> mythicPlusDungeonList = mapToMythicPlusDungeonList(node);
+
+        dungeonData.setMythicPlusDungeons(mythicPlusDungeonList);
+        return dungeonData;
+    }
+
+    private static List<MythicPlusDungeon> mapToMythicPlusDungeonList(JsonNode jsonResponseNode) {
+        List<MythicPlusDungeon> mythicPlusDungeonList = new ArrayList<>();
+
+        JsonNode parentNode = jsonResponseNode.get("best_runs");
+        for (int i = 0; i < parentNode.size(); i++) {
+            JsonNode childNode = parentNode.get(i);
+
+            LocalDateTime completedDate = LocalDateTime.ofInstant(Instant.ofEpochMilli(childNode.get("completed_timestamp").asLong()), ZoneId.systemDefault());
+            LocalTime duration = LocalTime.ofSecondOfDay(childNode.get("duration").asLong() / 1000);
+//            LocalTime timer;
+
+            Set<KeystoneAffix> keystoneAffixes = mapToKeystoneAffixSet(childNode);
+
+            MythicPlusDungeon dungeon = MythicPlusDungeon.builder()
+                    .id(childNode.get("dungeon").get("id").asInt())
+                    .name(childNode.get("dungeon").get("name").asText())
+                    .isCompletedWithinTime(childNode.get("is_completed_within_time").asBoolean())
+                    .keystoneLevel(childNode.get("keystone_level").asInt())
+                    .completedDate(completedDate)
+                    .duration(duration)
+                    .timer(duration) //subject to change
+                    .affixes(keystoneAffixes)
+                    .build();
+            mythicPlusDungeonList.add(dungeon);
+        }
+        return mythicPlusDungeonList;
+    }
+
+    private static Set<KeystoneAffix> mapToKeystoneAffixSet(JsonNode bestRunElement) {
+        Set<KeystoneAffix> keystoneAffixes = new HashSet<>();
+
+        for (int i = 0; i < bestRunElement.get("keystone_affixes").size(); i++) {
+            JsonNode affixNode = bestRunElement.get("keystone_affixes").get(i);
+            KeystoneAffix affix = KeystoneAffix.builder()
+                    .id(affixNode.get("id").asInt())
+                    .href(affixNode.get("key").get("href").asText())
+                    .name(affixNode.get("name").asText())
+                    .build();
+
+            keystoneAffixes.add(affix);
+        }
+        return keystoneAffixes;
+    }
+
+
+}
--- src/main/java/com/maciek/warcraftstatstracker/service/DungeoneerService.java
+++ src/main/java/com/maciek/warcraftstatstracker/service/DungeoneerService.java
@@ -1,25 +1,13 @@
 package com.maciek.warcraftstatstracker.service;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.maciek.warcraftstatstracker.mapper.DungeonDataMapper;
 import com.maciek.warcraftstatstracker.model.dungeoneer.DungeonData;
-import com.maciek.warcraftstatstracker.model.dungeoneer.KeystoneAffix;
-import com.maciek.warcraftstatstracker.model.dungeoneer.MythicPlusDungeon;
 import com.maciek.warcraftstatstracker.service.api.BlizzardApiService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.stereotype.Service;
-
-import java.time.Instant;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
-import java.time.ZoneId;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
 
 @Service
 public class DungeoneerService {
@@ -35,49 +23,6 @@ public class DungeoneerService {
         ResponseEntity<String> response = blizzardApiService.getRequestBlizzardApi("https://eu.api.blizzard.com/profile/wow/character/" + realm + "/" + characterName + "/mythic-keystone-profile/season/" + season + "?namespace=profile-eu&locale=en_US",
                 String.class, oAuth2Authentication);
 
-        DungeonData dungeonData = new DungeonData();
-        List<MythicPlusDungeon> mythicPlusDungeonList = new ArrayList<>();
-
-        ObjectMapper objectMapper = new ObjectMapper();
-        JsonNode node = objectMapper.readValue(response.getBody(), JsonNode.class);
-
-        dungeonData.setSeason(node.get("season").get("id").asInt());
-
-        JsonNode parentNode = node.get("best_runs");
-
-        for (int i = 0; i < parentNode.size(); i++) {
-            JsonNode childNode = parentNode.get(i);
-
-            LocalDateTime completedDate = LocalDateTime.ofInstant(Instant.ofEpochMilli(childNode.get("completed_timestamp").asLong()), ZoneId.systemDefault());
-            LocalTime duration = LocalTime.ofSecondOfDay(childNode.get("duration").asLong() / 1000);
-//            LocalTime timer;
-
-            Set<KeystoneAffix> keystoneAffixes = new HashSet<>();
-
-            for (int j = 0; j < childNode.get("keystone_affixes").size(); j++) {
-                JsonNode affixNode = childNode.get("keystone_affixes").get(j);
-                KeystoneAffix affix = KeystoneAffix.builder()
-                        .id(affixNode.get("id").asInt())
-                        .href(affixNode.get("key").get("href").asText())
-                        .name(affixNode.get("name").asText())
-                        .build();
-
-                keystoneAffixes.add(affix);
-            }
-
-            MythicPlusDungeon dungeon = MythicPlusDungeon.builder()
-                    .id(childNode.get("dungeon").get("id").asInt())
-                    .name(childNode.get("dungeon").get("name").asText())
-                    .isCompletedWithinTime(childNode.get("is_completed_within_time").asBoolean())
-                    .keystoneLevel(childNode.get("keystone_level").asInt())
-                    .completedDate(completedDate)
-                    .duration(duration)
-                    .timer(duration) //subject to change
-                    .affixes(keystoneAffixes)
-                    .build();
-            mythicPlusDungeonList.add(dungeon);
-        }
-        dungeonData.setMythicPlusDungeons(mythicPlusDungeonList);
-        return dungeonData;
+        return DungeonDataMapper.mapJSONToDungeonData(response.getBody());
     }
 }
