2c317c64833b585d8f623a7bb77654763151c355
m9ciek <ahubu123@gmail.com>
2020-09-08 11:57:09 +0200

    Implemented first version of score calculation

--- src/main/java/com/maciek/warcraftstatstracker/service/DungeoneerService.java
+++ src/main/java/com/maciek/warcraftstatstracker/service/DungeoneerService.java
@@ -3,11 +3,14 @@ package com.maciek.warcraftstatstracker.service;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.maciek.warcraftstatstracker.mapper.DungeonDataMapper;
 import com.maciek.warcraftstatstracker.model.dungeoneer.DungeonData;
+import com.maciek.warcraftstatstracker.model.dungeoneer.MythicPlusDungeon;
 import com.maciek.warcraftstatstracker.service.api.BlizzardApiService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalTime;
 
 @Service
 public class DungeoneerService {
@@ -23,6 +26,25 @@ public class DungeoneerService {
         ResponseEntity<String> response = blizzardApiService.getRequestBlizzardApi("https://eu.api.blizzard.com/profile/wow/character/" + realm + "/" + characterName + "/mythic-keystone-profile/season/" + season + "?namespace=profile-eu&locale=en_US",
                 String.class, oAuth2Authentication);
 
-        return DungeonDataMapper.mapJSONToDungeonData(response.getBody());
+        DungeonData playerDungeonData = DungeonDataMapper.mapJSONToDungeonData(response.getBody());
+        playerDungeonData.getMythicPlusDungeons()
+                .forEach(e -> e.setScore(calculateDungeonScore(e)));
+        return playerDungeonData;
+    }
+
+    private double calculateDungeonScore(MythicPlusDungeon mythicPlusDungeon) {
+        LocalTime timer = mythicPlusDungeon.getTimer();
+        LocalTime completedDuration = mythicPlusDungeon.getDuration();
+        int keystoneLevel = mythicPlusDungeon.getKeystoneLevel();
+        double dungeonScore = keystoneLevel * 10.0;
+
+        if (mythicPlusDungeon.isCompletedWithinTime()) {
+            int secondsDiff = timer.toSecondOfDay() - completedDuration.toSecondOfDay();
+            dungeonScore += (secondsDiff / 1.0) * 0.1; //each 0.1 point == 1.0 sec.
+        } else {
+            int secondsDiff = completedDuration.toSecondOfDay() - timer.toSecondOfDay();
+            dungeonScore -= (secondsDiff / 1.0) * 0.1;
+        }
+        return Math.round(dungeonScore * 10.0) / 10.0; //round up to 1 decimal place
     }
 }
